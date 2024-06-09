44d65c27cfce13ef5e0fa3c71b7b342c006106f1
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-06-09 08:29:42 +0200

    routing when logging out

--- app/components/logout.tsx
+++ app/components/logout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/navigation";
 import styles from "./logout.module.css";
 
 const Logout: React.FC = () => {
   const { logout } = useAuth();
+  const router = useRouter();
 
   const handleLogout = () => {
     logout();
+    router.push("/"); 
   };
 
   return (
